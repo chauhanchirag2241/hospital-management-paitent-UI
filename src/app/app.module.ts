78d0040e827beaf8f8f621d186dc208a6138d934
chirag <coderchirag2272@gmail.com>
2024-02-04 00:02:12 +0530

    add paitent form and navigation bar and material library and bootstrap cdn.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PaitentComponent } from './paitent/paitent.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatNativeDateModule } from '@angular/material/core';
 
+import { MatSelectModule } from '@angular/material/select';
+//import { provideNativeDateAdapter } from '@angular/material/core';
+//import { MatMomentDateModule } from "@angular/material-moment-adapter";
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +28,21 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatStepperModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    //MatMomentDateModule
+    //provideNativeDateAdapter
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/paitent/paitent.component.ts
+++ src/app/paitent/paitent.component.ts
@@ -1,4 +1,6 @@
+import { getLocaleDateTimeFormat } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-paitent',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./paitent.component.css']
 })
 export class PaitentComponent {
+  minDate: Date;
+  maxDate: Date;
+
+  constructor(private _formBuilder: FormBuilder) {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 20, 0, 1);
+    this.maxDate = new Date(currentYear + 1, 11, 31); }
+  paitentFormGroup = this._formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', [Validators.required]],
+    mobileNo: ['', [Validators.required]],
+    emgMobileNo: ['', [Validators.required]],
+    birthDate: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    gender: ['', [Validators.required]],
+    address: ['', [Validators.required]],
+    booldGroup: ['', [Validators.required]],
+    medicalIssue: ['', [Validators.required]],
+    doctorid: ['', [Validators.required]],
+    visiteDate: ['', [Validators.required]],
+    visiteTime: ['', [Validators.required]],
+
+  });
+
+
+  makeAppointment() {
+    console.log(this.paitentFormGroup.value);
+  }
 
 }
